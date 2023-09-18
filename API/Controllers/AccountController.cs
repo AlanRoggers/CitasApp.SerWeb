0012a962ac6bad2ab4408c0a0e3596aa1a3d43eb
AlanRoggers <alanroggersmp@outlook.com>
2023-09-18 08:09:19 -0600

    [18/09/2023] Dtos

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -14,18 +16,25 @@ namespace API.Controllers
             _context = context;
         }
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.Username))
+                return BadRequest("Ya existe el nombre de usuario");
+
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
